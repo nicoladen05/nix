dea5ebab5d909e0a515c89341ac641317fe6ac25
nicoladen05 <nicolashartmanntaba@gmail.com>
2024-08-17 17:51:25 +0200

    commit

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -9,7 +9,10 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       inputs.home-manager.nixosModules.default
+
+      ../../modules/nixos/hyprland.nix
     ];
+
 
   # Flakes
   nix.settings.experimental-features = ["nix-command" "flakes" ];
@@ -61,18 +64,18 @@
 
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "colemak";
-    options = "caps:swapescape";
-  };
+  # services.xserver.xkb = {
+  #   layout = "us";
+  #   variant = "colemak";
+  #   options = "caps:swapescape";
+  # };
 
   # Enable CUPS to print documents.
   services.printing.enable = false;
