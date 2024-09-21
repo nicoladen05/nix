5ba4e19b390195dbf2523855ca88f51414542e4f
nicoladen05 <nicolashartmanntaba@gmail.com>
2024-09-21 09:38:57 +0200

    remove neovim

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -121,7 +121,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    neovim
+    # neovim
     git
     stow
     gcc
