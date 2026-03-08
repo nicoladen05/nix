5472711673dede9fe60e4d3ed0e860fc44517442
nicoladen05 <nicolashartmanntaba@gmail.com>
2026-03-08 19:28:08 +0100

    disable xone

--- modules/nixos/gaming/controller.nix
+++ modules/nixos/gaming/controller.nix
@@ -3,17 +3,17 @@
   lib,
   config,
   ...
-}: {
+}:
+{
   options = {
     gaming.controller.xbox.enable = lib.mkEnableOption "enable xbox one controller";
   };
 
   config = lib.mkIf config.gaming.controller.xbox.enable {
     hardware.xpadneo.enable = true;
-    hardware.xone.enable = true;
 
     boot = {
-      extraModulePackages = with config.boot.kernelPackages; [xpadneo];
+      extraModulePackages = with config.boot.kernelPackages; [ xpadneo ];
       extraModprobeConfig = ''
         options bluetooth disable_ertm=Y
       '';
