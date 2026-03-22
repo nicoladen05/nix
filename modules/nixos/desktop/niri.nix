b01fa856f6a963de77a510ed939a3ceecc1c84f0
nicoladen05 <nicolashartmanntaba@gmail.com>
2026-03-22 09:38:51 +0100

    add nvidia environment variables for niri session

--- modules/nixos/desktop/niri.nix
+++ modules/nixos/desktop/niri.nix
@@ -12,6 +12,15 @@
   config = lib.mkIf config.desktop.niri.enable {
     programs.niri.enable = true;
 
+    environment.sessionVariables = lib.mkIf config.system.nvidia.enable {
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      __GL_DXVK_USE_LAST_PIPELINE_CACHE = "1";
+      __GL_GSYNC_ALLOWED = "1";
+      LIBVA_DRIVER_NAME = "nvidia";
+
+      NIXOS_OZONE_WL = "1";
+    };
+
     # Keyring
     services.gnome.gnome-keyring.enable = true;
     security.pam.services.login.enableGnomeKeyring = true;
