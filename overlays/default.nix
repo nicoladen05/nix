dd114019f48441b69776fc1ea89af3b72dde916f
nicoladen05 <nicolashartmanntaba@gmail.com>
2026-03-24 16:36:35 +0100

    caddy overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,9 @@
 final: prev: rec {
+  caddy = prev.caddy.overrideAttrs (_: previousAttrs: {
+    go = final.go_1_26;
+    nativeBuildInputs = builtins.filter (input: input != prev.go) previousAttrs.nativeBuildInputs ++ [ final.go_1_26 ];
+  });
+
   python313 = prev.python313.override {
     packageOverrides = pythonFinal: pythonPrev: {
       pycord = prev.callPackage ../packages/pycord.nix {};
