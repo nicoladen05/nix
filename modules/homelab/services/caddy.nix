cfa038f81cdb512e871a1976d70b8686d980cef1
nicoladen05 <nicolashartmanntaba@gmail.com>
2026-03-24 16:27:08 +0100

    update caddy hash

--- modules/homelab/services/caddy.nix
+++ modules/homelab/services/caddy.nix
@@ -13,7 +13,7 @@
       email = "acme@nicoladen.dev";
 
       package = pkgs.caddy.withPlugins {
-        plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
+        plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
         hash = "sha256-Olz4W84Kiyldy+JtbIicVCL7dAYl4zq+2rxEOUTObxA=";
       };
 
