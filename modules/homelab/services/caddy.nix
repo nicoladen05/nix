365b1b42b3c6aac389365a0f5e49ab5cedb6dce0
nicoladen05 <nicolashartmanntaba@gmail.com>
2026-03-24 14:07:39 +0100

    update caddy plugin

--- modules/homelab/services/caddy.nix
+++ modules/homelab/services/caddy.nix
@@ -13,8 +13,8 @@
       email = "acme@nicoladen.dev";
 
       package = pkgs.caddy.withPlugins {
-        plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-        hash = "sha256-biQdtiscdmrwP6VUsuXmZrvcdewl+g50kdmab5lhE0s=";
+        plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
+        hash = "sha256-0Csi6WmyoGj7bXeo2Lrnwr0SCoV6c/niymtOp5DdiT4=";
       };
 
       environmentFile = config.sops.templates."caddy-env".path;
