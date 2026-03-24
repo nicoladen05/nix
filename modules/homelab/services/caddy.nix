142a8e8725575c80157a2d9e22b5cb336c166d43
nicoladen05 <nicolashartmanntaba@gmail.com>
2026-03-24 16:29:12 +0100

    update caddy hash

--- modules/homelab/services/caddy.nix
+++ modules/homelab/services/caddy.nix
@@ -14,7 +14,7 @@
 
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-        hash = "sha256-Olz4W84Kiyldy+JtbIicVCL7dAYl4zq+2rxEOUTObxA=";
+        hash = "sha256-7DGnojZvcQBZ6LEjT0e5O9gZgsvEeHlQP9aKaJIs/Zg=";
       };
 
       environmentFile = config.sops.templates."caddy-env".path;
