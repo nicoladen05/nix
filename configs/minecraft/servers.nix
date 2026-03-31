29adeb98fdbeba658e1fba7bc15713262bf150e7
nicoladen05 <nicolashartmanntaba@gmail.com>
2026-03-31 19:25:37 +0200

    add psychedelicraft

--- configs/minecraft/servers.nix
+++ configs/minecraft/servers.nix
@@ -24,6 +24,7 @@ in
         many-more-ores-and-crafts = mods.many-more-ores-and-crafts;
         ultimate-plane-mod = mods.ultimate-plane-mod;
         falling-tree = mods.falling-tree;
+        psychedelicraft = mods.psychedelicraft;
       };
     };
   };
